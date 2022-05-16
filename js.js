753ef1bba6e930e9e44115609ba67ed161c60068
matheusxds123 <101800697+matheusxds123@users.noreply.github.com>
2022-05-15 22:22:36 -0300

    Setup JS e inclusão do axios

--- js.js
+++ js.js
@@ -6,6 +6,24 @@ document.querySelector("#btEntrar").addEventListener("click", function () {
     let password = document.querySelector("#password");
     if(username.value == localStorage.getItem("user") && password.value == localStorage.getItem("pw")){
         alert("Login realizado com sucesso!");
+        let sectionLogin = document.querySelector(".login");
+        sectionLogin.innerHTML = "";
+        sectionLogin.style.display = "none";
+        let txt = document.createElement("h4");
+        txt.innerHTML = "Entre com o nome do pokémon:";
+        document.body.appendChild(txt);
+        let br = document.createElement("br");
+        document.body.appendChild(br);
+        let input = document.createElement("input");
+        document.body.appendChild(input);
+        let button = document.createElement("button");
+        button.innerHTML = "Pesquisar";
+        document.body.appendChild(button);
+        let list = document.createElement("ul");
+        for(let i = 0; i < 10; i++){
+            let search = document.createElement("li");
+            let text = document.createTextNode("Teste");
+        }
         
     } else{
         alert("Usuário ou senha inválidos! (Olhar console)");
